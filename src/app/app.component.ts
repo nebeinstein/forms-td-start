107e75b6c4cef90d638e2b43be7d61bfe310015d
jack rivadeneira <jrrivadeneira@wpi.edu>
2017-06-28 12:40:21 -0400

    finished the TD forms section of the tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,37 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  defaultAnswer = '';
+  genders = ['male', 'female', 'other'];
+  user = {
+	  username: '',
+	  email: '',
+	  secretQuestion: '',
+	  answer: '',
+	  gender: ''
+  }
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+	// setValue() overrides and sets the entire form.
+	// this.signupForm.setValue({
+	// 	userData: {
+	// 		username: suggestedName,
+	// 		email: ''
+	// 	},
+	// 	secret: 'pet',
+	// 	questionAnswer: '',
+	// 	gender: 'male'
+	// });
+
+	// patchValue() sets only parts of the form.
+	this.signupForm.form.patchValue({
+		userData: {
+			username: suggestedName
+		}
+	});
   }
 
   // onSubmit(form: NgForm){
@@ -18,7 +46,14 @@ export class AppComponent {
   // }
 
   onSubmit(){
-    console.log(this.signupForm);
+	  this.submitted = true;
+	  this.user.username = this.signupForm.value.userData.username;
+	  this.user.email = this.signupForm.value.userData.email;
+	  this.user.secretQuestion = this.signupForm.value.secret;
+	  this.user.answer = this.signupForm.value.questionAnswer;
+	  this.user.gender = this.signupForm.value.gender;
+	  
+	  this.signupForm.reset();
   }
 
 }
